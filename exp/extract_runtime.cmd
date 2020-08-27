4d9d37e177fbcf05caf28b5583f609257e5d0703
Martin Li <martinli.work@gmail.com>
2020-08-27 01:31:32 -0400

    Update extract_runtime.cmd

--- exp/extract_runtime.cmd
+++ exp/extract_runtime.cmd
@@ -9,5 +9,5 @@ set platform=1080ti
 
 python util\add_to_runtime_zoo.py ^
 	--time-info "%dataDir%\Exp\Argoverse-HD\output\rt_%methodName%_s%scale%\val\time_info.pkl" ^
-	--out-path "%dataDir%\Exp\Argoverse-HD\runtime-zoo\$platform\%methodName%_s%scale%.pkl" ^
+	--out-path "%dataDir%\Exp\Argoverse-HD\runtime-zoo\%platform%\%methodName%_s%scale%.pkl" ^
 	--overwrite ^
